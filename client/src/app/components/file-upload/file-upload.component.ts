27eeedd67ec511e2cd1749c234d90f2ad57ce244
Kvalle99 <sebastian.kvalden@gmail.com>
2023-02-06 11:29:00 +0100

    created endpoint

--- client/src/app/components/file-upload/file-upload.component.ts
+++ client/src/app/components/file-upload/file-upload.component.ts
@@ -1,69 +1,64 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-
+import { API_URL } from 'src/environments/environment';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+  styleUrls: ['./file-upload.component.css'],
 })
-export class FileUploadComponent  {
-  files : any [] = [];
-  @ViewChild("fileUpload", { static: false })
+export class FileUploadComponent {
+  files: any[] = [];
+  @ViewChild('fileUpload', { static: false })
   fileDropEl!: ElementRef;
 
-  constructor(private http : HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  onFileDropped($event : any) : void{
+  onFileDropped($event: any): void {
     this.prepareFilesList($event);
   }
 
-  fileBrowseHandler(files : any) : void {
+  fileBrowseHandler(files: any): void {
     this.prepareFilesList(files.target.files);
   }
 
-  deleteFile(index: number) : void {
+  deleteFile(index: number): void {
     this.files.splice(index, 1);
   }
 
-  prepareFilesList(files: Array<any>) : void {
+  prepareFilesList(files: Array<any>): void {
     for (const item of files) {
       this.files.push(item);
     }
-    this.fileDropEl.nativeElement.value = "";
+    this.fileDropEl.nativeElement.value = '';
   }
 
-  formatBytes(bytes : any) : string {
+  formatBytes(bytes: any): string {
     if (bytes === 0) {
-      return "0 Bytes";
+      return '0 Bytes';
     }
     const k = 1024;
-    const dm = 2
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const dm = 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 
   uploadFiles() {
     const formData = new FormData();
-    this.files.forEach(file => formData.append('files', file, file.name));
+    this.files.forEach((file) => formData.append('files', file, file.name));
+    //formData.append('files', this.files[0], this.files[0].name);
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'multipart/form-data'
-      })
-    };
+    formData.forEach((data) => console.log(data));
 
-    console.log("Upload in progress with: ", formData );
-    
     // TODO: Change to correct endpoint when it exists one.
-    this.http.post('/api/upload', formData, httpOptions).subscribe({
+    this.http.post(`${API_URL}/files`, formData).subscribe({
       next: (response) => {
         //TODO: Handle the success response
       },
       error: (err) => {
         //TODO: Handle the error
-      }
+      },
     });
   }
 }
--- server/src/app.py
+++ server/src/app.py
@@ -12,3 +12,19 @@ def testGet():
     #do the doings here
     return jsonify("hello")
 
+@app.route('/files', methods=['POST'])
+def testPOST(): 
+    #do the doings here
+    print('recieved request')
+    files = request.files.getlist('files')
+
+    if not files:
+        return "No file man"
+        
+    for file in files:
+        print('in loop')
+        print(file.filename)
+        file.save(file.filename)
+   # print(file)
+    return 'success' , 200
+
