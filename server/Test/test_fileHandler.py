4be054402a84d192fc4648d00f4ea113869f3156
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2023-02-08 21:08:16 +0100

    remove unnecessary imports and remove unused class `File` and add filename to the FileStorage

--- server/Test/test_fileHandler.py
+++ server/Test/test_fileHandler.py
@@ -5,53 +5,43 @@ sys.path.append(str(Path(__file__).absolute().parent.parent))
 
 import unittest
 from src.fileHandler import handle_files
-from src.app import post_files
 from werkzeug.datastructures import FileStorage
 
 class TestStringMethods(unittest.TestCase):
 
     def test_send_PDF_file(self):
-        with open("Test1.pdf") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"Test1.pdf", "rb") as fp:
+            file = FileStorage(fp, filename="Test1.pdf")
         files=[file]
-        print(handle_files(files)[1])
         self.assertEqual(handle_files(files)[1], 200)
 
     def test_send_txt_file(self):
-        with open("test2.txt") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"test2.txt", "rb") as fp:
+            file = FileStorage(fp, filename="test2.txt")
         files=[file]
         self.assertEqual(handle_files(files)[1], 200)
     
     def test_send_PY_file(self):
-        with open("PythonFile.py") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"PythonFile.py", "rb") as fp:
+            file = FileStorage(fp, filename="PythonFile.py")
         files=[file]
         self.assertEqual(handle_files(files)[1], 200)
 
     def test_send_multiple_files(self):
         files=[]
-        with open("Test1.pdf") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"Test1.pdf", "rb") as fp:
+            file = FileStorage(fp, filename="Test1.pdf")
             files.append(file)
-        with open("test2.txt") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"test2.txt", "rb") as fp:
+            file = FileStorage(fp, filename="test2.txt")
             files.append(file)
         
-        with open("PythonFile.py") as fp:
-            file = FileStorage(fp)
+        with open(Path(__file__).absolute().parent/"PythonFile.py", "rb") as fp:
+            file = FileStorage(fp, filename="PythonFile.py")
             files.append(file)
         print(len(files))
         self.assertEqual(handle_files(files)[1], 406) #only allows one file at the moment
 
-    #def test_post_no_files(self):
-     #   self.assertEqual(post_files()[1],406)
-
-class File:
-    filename:str
-
-    def __init__(name:str):
-        filename = name
 
 if __name__ == '__main__':
     unittest.main()
