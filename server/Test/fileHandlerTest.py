081ff141c63a41f501a5a62066602a0a3f759d08
Kvalle99 <sebastian.kvalden@gmail.com>
2023-02-08 09:27:46 +0100

    Created tests for different filetypes

--- server/Test/fileHandlerTest.py
+++ server/Test/fileHandlerTest.py
@@ -1,7 +1,3 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).absolute().parent.parent))
 
 import unittest
 from src.fileHandler import handle_files
@@ -9,7 +5,20 @@ from src.fileHandler import handle_files
 class TestStringMethods(unittest.TestCase):
 
     def test_send_PDF_file(self):
-        self.assertEqual(handle_files("Test1.pdf")[1], 200)
+        files=[open("Test1.pdf")]
+        self.assertEqual(handle_files(files)[1], 200)
+
+    def test_send_txt_file(self):
+        files=[open("test2.txt")]
+        self.assertEqual(handle_files(files)[1], 200)
+    
+    def test_send_PY_file(self):
+        files=[open("fileHandlerTest.py")]
+        self.assertEqual(handle_files(files)[1], 200)
+
+    def test_send_multiple_files(self):
+        files = [open("Test1.pdf"), open("test2.txt"),open("fileHandlerTest.py")]
+        self.assertEqual(handle_files(files)[1], 200)
 
 
 if __name__ == '__main__':
--- server/src/fileHandler.py
+++ server/src/fileHandler.py
@@ -1,3 +1,5 @@
+
+
 from flask import jsonify
 from werkzeug.utils import secure_filename
 
@@ -15,15 +17,15 @@ def handle_files(files):
     response_args = {}
     res_code = 200
 
-    if not(len(files) == nr_of_files):
-        response_args.update({"Wrong amount of files": "Recieved " + str(len(files)) + ", should be " + str(nr_of_files) + " files"})
+    if not(len(files) == __nr_of_files):
+        response_args.update({"Wrong amount of files": "Recieved " + str(len(files)) + ", should be " + str(__nr_of_files) + " files"})
     
     for file in files:
         res_object = {}
 
         file.filename = secure_filename(file.filename)
         
-        if not (file.filename in allowed_filenames):
+        if not (file.filename in __allowed_filenames):
             res_object.update({"File Name": "Not allowed file name"})
             res_code = 406
         else: 
@@ -44,4 +46,4 @@ def handle_files(files):
 #Method to check file extension for allowed files
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in __ALLOWED_EXTENSIONS
