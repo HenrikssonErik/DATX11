9a271c5c975fc1b3f707cdfcc5ea0ef8a6699c9b
Kvalle99 <sebastian.kvalden@gmail.com>
2023-02-08 17:06:22 +0100

    removed old comments

--- server/src/app.py
+++ server/src/app.py
@@ -1,19 +1,16 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
-#could be used to sanitize file names but TAs will decide allowed filenamnes so it is probably not needed
 
 from .fileHandler import handle_files
 
 # creating the Flask application
 app = Flask(__name__)
 
-#made need to change
 CORS(app)
 
 #useless in the future, TODO: Remove along with front end button
 @app.route('/test', methods=['GET'])
 def testGet(): 
-    #do the doings here
     return jsonify("hello")
 
 @app.route('/files', methods=['POST'])
