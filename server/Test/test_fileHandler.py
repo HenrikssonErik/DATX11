d44bf83eec31fe68517d00d9f6d96b648b15ee7c
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2023-02-15 21:22:28 +0100

    pep8 conventions fixed

--- server/Test/test_fileHandler.py
+++ server/Test/test_fileHandler.py
@@ -1,45 +1,50 @@
 import sys
 from pathlib import Path
+import unittest
+from werkzeug.datastructures import FileStorage
 
 sys.path.append(str(Path(__file__).absolute().parent.parent))
+from src.fileHandler import handle_files  # noqa: E402
 
-import unittest
-from src.fileHandler import handle_files
-from werkzeug.datastructures import FileStorage
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_send_PDF_file(self):
+    def test_send_pdf_file(self):
         with open(Path(__file__).absolute().parent/"Test1.pdf", "rb") as fp:
             file = FileStorage(fp, filename="Test1.pdf")
-        files=[file]
+        files = [file]
         self.assertEqual(handle_files(files)[1], 200)
 
     def test_send_txt_file(self):
         with open(Path(__file__).absolute().parent/"test2.txt", "rb") as fp:
             file = FileStorage(fp, filename="test2.txt")
-        files=[file]
+        files = [file]
         self.assertEqual(handle_files(files)[1], 200)
-    
-    def test_send_PY_file(self):
-        with open(Path(__file__).absolute().parent/"PythonFile.py", "rb") as fp:
+
+    def test_send_py_file(self):
+        with open(
+            Path(__file__).absolute().parent/"PythonFile.py", "rb"
+        ) as fp:
             file = FileStorage(fp, filename="PythonFile.py")
-        files=[file]
+        files = [file]
         self.assertEqual(handle_files(files)[1], 200)
 
     def test_send_multiple_files(self):
-        files=[]
+        files = []
         with open(Path(__file__).absolute().parent/"Test1.pdf", "rb") as fp:
             file = FileStorage(fp, filename="Test1.pdf")
             files.append(file)
         with open(Path(__file__).absolute().parent/"test2.txt", "rb") as fp:
             file = FileStorage(fp, filename="test2.txt")
             files.append(file)
-        
-        with open(Path(__file__).absolute().parent/"PythonFile.py", "rb") as fp:
+
+        with open(
+            Path(__file__).absolute().parent/"PythonFile.py", "rb"
+        ) as fp:
             file = FileStorage(fp, filename="PythonFile.py")
             files.append(file)
-        self.assertEqual(handle_files(files)[1], 406) #only allows one file at the moment
+        # only allows one file at the moment
+        self.assertEqual(handle_files(files)[1], 406)
 
 
 if __name__ == '__main__':
