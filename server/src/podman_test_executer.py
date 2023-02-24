200b74421fc26eafb852e8881b4fb7b747ba1794
alfkar <alfkar@student.chalmers.se>
2023-02-24 14:40:38 +0100

    Co-authored-by: GabbeHags <GabbeHags@users.noreply.github.com>

---
+++ server/src/podman_test_executer.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import unittest
+import json
+import sys
+
+if __name__ == "__main__":
+    # test_dir = sys.argv[1]
+    test_dir = str(Path(__file__).absolute().parent/"DATX11")
+    print(test_dir)
+
+    test_suit = unittest.TestLoader().discover(
+        test_dir, pattern="test_*.py"
+    )
+    results = test_suit.run(unittest.TestResult(verbosity=2))
+    json_result = {
+        "tests_run": results.testsRun,
+        "was_successful": results.wasSuccessful(),
+        "errors": [(e[0].id(), e[1]) for e in results.errors],
+        "failures": [(e[0].id(), e[1]) for e in results.failures],
+        "expected_failures": [
+            (e[0].id(), e[1]) for e in results.expectedFailures
+        ],
+        "unexpected_successes": [
+            e.id() for e in results.unexpectedSuccesses
+        ],
+        "skipped": [(e[0].id(), e[1]) for e in results.skipped],
+    }
+
+    print(json.dumps(json_result))
+
+
+    
