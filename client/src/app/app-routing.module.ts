f0e370ee682597661f1e6a327bfb379a17e39d19
maltecarlstedt <malte.carlstedt@hotmail.se>
2023-02-26 15:05:58 +0100

    [FRONTEND] added routing to login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  { path: 'login', component: LoginComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
