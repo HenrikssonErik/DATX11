27cf7dcfd9c17377f1db9ca0858ce124928a462d
Kvalle99 <sebastian.kvalden@gmail.com>
2023-02-27 15:59:42 +0100

    created function to hash password with salt before call to backend

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -4,49 +4,56 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm: FormGroup = new FormGroup({});
   submitFailed: boolean = false;
   success: boolean = false;
+  bcrypt = require('bcryptjs');
 
-  constructor(private fb: FormBuilder) {
-  
-  }
-  
-
-
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
-      rememberMe: [false]
+      rememberMe: [false],
     });
-  
   }
 
   onSubmit() {
+    console.log('onSubmit');
     if (this.loginForm.invalid) {
       this.submitFailed = true;
+
       return;
     }
+    console.log(this.loginForm.get('password')?.value);
+
+    const hashedpassword = this.hashPassword(
+      this.loginForm.get('password')?.value
+    );
 
     // Logic to submit form data to server
+    //TODO: create method to hash password with bcrypt(done) -> send to backend -> handle response with/without token
 
     this.success = true;
   }
 
+  hashPassword(password: string): string {
+    const hash: string = this.bcrypt.hashSync(password, 10);
+    return hash;
+  }
+
   onInputFocus(input: string) {
     const control = this.loginForm.get(input);
-    
+
     if (control) {
       const isValid = control.valid;
       const isInvalid = control.invalid && (control.dirty || control.touched);
       const el = document.getElementById(input);
-  
+
       if (isValid) {
         el?.classList.add('success');
         el?.classList.remove('error');
@@ -59,4 +66,4 @@ export class LoginComponent implements OnInit {
       }
     }
   }
-}  
+}
