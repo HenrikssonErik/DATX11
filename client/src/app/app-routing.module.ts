271e8af3866e3adcff357a4f97552b2fdc11809b
Alexander Brunnegård <alex_brunnegard98@hotmail.com>
2023-03-02 11:35:19 +0100

    Automatic formatting via prettier. Removed path to not show nav and footer twice.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
-  { path: 'login', component: LoginComponent }
-  
+  /** THIS path seems to be the reason for the duplication of both the nav and footer.
+   * Though, I do not know if this would be a good fix to remove this path entirely.
+   * */
+  /* { path: '', component: AppComponent }, */
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
