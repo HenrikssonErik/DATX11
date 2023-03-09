f55df270b8c9c79ff0aec63a21649499f59b3be8
Alexander Brunnegård <alex_brunnegard98@hotmail.com>
2023-03-09 10:01:51 +0100

    Started making tests for user_registration

--- server/Test/test_login_handler.py
+++ server/Test/test_login_handler.py
@@ -3,10 +3,9 @@ from pathlib import Path
 import unittest
 from unittest.mock import Mock, patch
 import bcrypt
-
+from src.login_handler import log_in, check_data_input
 
 sys.path.append(str(Path(__file__).absolute().parent.parent))
-from src.login_handler import log_in, verify_token, create_token
 
 
 class TestFileHandler(unittest.TestCase):
@@ -15,6 +14,31 @@ class TestFileHandler(unittest.TestCase):
         self.test_file_dir = Path(__file__).parent/"test_files_file_handler"
 
     @patch('app.psycopg2.connect')
+    def test_check_data_input(self, mock_conn):
+        # Test case where input data is valid
+        self.assertEqual(check_data_input(
+            'abc', 'abc@chalmers.se', '1234'), ('OK', 200))
+
+        # Test case where cid contains invalid characters
+        self.assertEqual(check_data_input(
+            '123', 'abc@chalmers.se', '1234'), ('unallowed_tokens', 400))
+
+        # Test case where cid is missing
+        self.assertEqual(check_data_input(
+            '', 'abc@chalmers.se', '1234'), ('cid_missing', 400))
+
+        # Test case where email is missing
+        self.assertEqual(check_data_input(
+            'abc', '', '1234'), ('email_missing', 400))
+
+        # Test case where email format is wrong
+        self.assertEqual(check_data_input(
+            'abc', 'def@chalmers.se', '1234'), ('wrong_format', 400))
+
+        # Test case where password contains invalid characters
+        self.assertEqual(check_data_input(
+            'abc', 'abc@chalmers.se', 'abc'), ('pass_not_ok', 400))
+
     def test_log_in(self, mock_connect):
         with patch('psycopg2.connect') as mock_connect:
             # Set up the mock return value
