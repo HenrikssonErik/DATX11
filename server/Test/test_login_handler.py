e284a2e99cb2c71865dcf2d1ee251d693d775f35
Alexander Brunnegård <alex_brunnegard98@hotmail.com>
2023-03-10 12:15:06 +0100

    Finalized tests of registration_query. Refactoring. Type-checking in except in login_handler

--- server/Test/test_login_handler.py
+++ server/Test/test_login_handler.py
@@ -1,12 +1,23 @@
 from src.login_handler import log_in, verify_token, create_token, create_key, \
-    check_data_input, user_registration
+    check_data_input, user_registration, registration_query
 import sys
 from pathlib import Path
 import unittest
 from unittest.mock import MagicMock, patch
 import bcrypt
+from psycopg2 import IntegrityError
+
 
 sys.path.append(str(Path(__file__).absolute().parent.parent))
+
+
+def setup_mock_cursor(mock_connect) -> MagicMock:
+    mock_cursor = MagicMock()
+    mock_conn = MagicMock()
+    mock_connect.return_value = mock_conn
+    mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
+
+    return mock_cursor
 
 
 class TestFileHandler(unittest.TestCase):
@@ -37,45 +48,75 @@ class TestFileHandler(unittest.TestCase):
             'abc', 'abc@chalmers.se', 'أَلِف'), ('pass_not_ok', 400))
 
     @patch('psycopg2.connect')
-    def test_user_registration(self, mock_connect):
+    def test_user_registration_success(self, mock_connect):
         with patch.object(bcrypt, 'gensalt') as mock_gensalt:
             mock_gensalt.return_value = b'$2b$12$5OYDyM.lB5wBLwMhFJj42O'
             salt = bcrypt.gensalt()
             passphrase = memoryview(bcrypt.hashpw('abc123'.encode('utf8'),
                                                   salt))
-            mock_cursor = MagicMock()
-            mock_conn = MagicMock()
-            mock_connect.return_value = mock_conn
-            mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
+            mock_cursor = setup_mock_cursor(mock_connect)
             mock_cursor.fetchone.return_value = [1, passphrase]
 
-            user_registration({'cid': 'abc', 'email': 'abc@chalmers.se',
-                               'password': 'abc123'})
+            result = user_registration({'cid': 'abc',
+                                        'email': 'abc@chalmers.se',
+                                        'password': 'abc123'})
+
+            self.assertEqual(result[1], 200)
+            self.assertTrue(result[0].get('Token').
+                            startswith("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"))
 
     @patch('psycopg2.connect')
+    def test_registration_query_success(self, mock_connect):
+        mock_cursor = setup_mock_cursor(mock_connect)
+
+        cid = 'abc'
+        email = 'abc@example.com'
+        hashed_pass = b'secret'
+
+        result = registration_query(cid, email, hashed_pass)
+        mock_cursor.execute.assert_called_once_with("""INSERT INTO UserData
+                (cid, email, passphrase)
+                VALUES (%s, %s, %s);""", (cid, email, hashed_pass))
+
+        self.assertEqual(result, ({'error': 'OK'}, 200))
+
+    @patch('psycopg2.connect')
+    def test_registration_query_unique_key_exception(self, mock_connect):
+        mock_cursor = setup_mock_cursor(mock_connect)
+        mock_cursor.execute.side_effect = IntegrityError(
+            "duplicate key value violates unique constraint 'userdata_pkey'"
+        )
+
+        cid = 'abc'
+        email = 'abc@example.com'
+        hashed_pass = b'secret'
+
+        result = registration_query(cid, email, hashed_pass)
+
+        mock_cursor.execute.assert_called_once_with("""INSERT INTO UserData
+                (cid, email, passphrase)
+                VALUES (%s, %s, %s);""", (cid, email, hashed_pass))
+
+        self.assertEqual(result, ({'error': 'already_registered'}, 406))
+
+    @ patch('psycopg2.connect')
     def test_sucessfull_log_in(self, mock_connect):
         # Set up the mock return value
         salt = bcrypt.gensalt()
         passphrase = memoryview(bcrypt.hashpw('pass'.encode('utf8'), salt))
-        mock_cursor = MagicMock()
-        mock_conn = MagicMock()
-        mock_connect.return_value = mock_conn
-        mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor = setup_mock_cursor(mock_connect)
         mock_cursor.fetchone.return_value = [1, passphrase]
         result = log_in('test1.chalmers.se', 'pass')
         self.assertEqual(result[1], 200)
         self.assertTrue(result[0].get('Token').startswith(
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'))
 
-    @patch('psycopg2.connect')
+    @ patch('psycopg2.connect')
     def test_unsucessfull_log_in(self, mock_connect):
         # Set up the mock return value
         salt = bcrypt.gensalt()
         passphrase = memoryview(bcrypt.hashpw('pass'.encode('utf8'), salt))
-        mock_cursor = MagicMock()
-        mock_conn = MagicMock()
-        mock_connect.return_value = mock_conn
-        mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor = setup_mock_cursor(mock_connect)
         mock_cursor.fetchone.return_value = [1, passphrase]
 
         result = log_in('test1.chalmers.se', 'pass1')
