7708c5b0a5299bd41b8ddb333888a3adfa37deac
Kvalle99 <sebastian.kvalden@gmail.com>
2023-03-15 15:17:58 +0100

    added method to retreive course info from userid

--- server/src/user_handler.py
+++ server/src/user_handler.py
@@ -2,7 +2,7 @@ from .connector import get_conn_string
 import psycopg2
 
 
-def get_courses(userId: int):
+def get_courses(userId: int) -> list[dict[str, any]]:
     conn = psycopg2.connect(dsn=get_conn_string())
 
     try:
@@ -12,10 +12,15 @@ def get_courses(userId: int):
                             WHERE userid = %s"""
                 cur.execute(query_data, (userId,))
                 data = cur.fetchall()
-                print(data)
         conn.close()
         if not data:
             raise Exception("No courses for this user")
+        orderedData: list[dict[str, any]] = []
+        for info in data:
+            orderedData.append({"Role": info[1], "courseID": info[2],
+                                "Course": info[3], "Year": info[4],
+                                "StudyPeriod": info[5]})
+        return orderedData
 
     except Exception as e:
         print(e)
