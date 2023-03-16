c16c6bbd337a84bd7ca75eef4021db17f8789a8f
Kvalle99 <sebastian.kvalden@gmail.com>
2023-03-16 15:36:28 +0100

    created method to add user to group

--- server/src/user_handler.py
+++ server/src/user_handler.py
@@ -2,10 +2,12 @@ from .connector import get_conn_string
 import psycopg2
 from enum import Enum
 
+
 class Role(Enum):
     Admin = 'Admin'
     Teacher = 'Teacher'
     Student = 'Student'
+
 
 def get_courses(userId: int) -> list[dict[str, any]]:
     conn = psycopg2.connect(dsn=get_conn_string())
@@ -29,10 +31,10 @@ def get_courses(userId: int) -> list[dict[str, any]]:
 
     except Exception as e:
         print(e)
-        return {'error': "No Courses Found"}, 401
+        return {'status': "No Courses Found"}, 401
 
 
-def get_course_group(userId: int, courseId: int) -> dict[str, str]:
+def get_group(userId: int, courseId: int) -> dict[str, str]:
     conn = psycopg2.connect(dsn=get_conn_string())
 
     try:
@@ -55,22 +57,62 @@ def get_course_group(userId: int, courseId: int) -> dict[str, str]:
 
     except Exception as e:
         print(e)
-        return {'error': "No Groups Found"}, 401
+        return {'status': "No Groups Found"}, 401
 
 # TODO: fill in the blanks :)
 
-def add_user_to_group(userId: int, groupId: int):
-    # check user on course
-    # check user is student
-    # check group on course
-    # add to group
 
+def add_user_to_group(userId: int, groupId: int):
+    # check user on course and group on the course
+    user_courses = get_courses(userId)
+    conn = psycopg2.connect(dsn=get_conn_string())
+
+    course_id = __get_courseId_from_group(groupId)
+
+    # add to group
+    try:
+        for course in user_courses:
+            if course['courseID'] == course_id and \
+               course['Role'] == Role.Student.name:
+                with conn:
+                    with conn.cursor() as cur:
+                        query_data = """INSERT into useringroup VALUES
+                                       (%s, %s)"""
+                        cur.execute(query_data, [userId, groupId])
+                conn.close()
+
+    except Exception as e:
+        print(e)
+        return {'status': "No Groups Found"}, 401
+
+
+def __get_courseId_from_group(groupId) -> int:
+    conn = psycopg2.connect(dsn=get_conn_string())
+
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                query_data = """SELECT course FROM
+                                groups
+                                WHERE groupid = %s """
+                cur.execute(query_data, [groupId])
+                data = cur.fetchone()
+
+        if not data:
+            raise Exception("No such group")
+        return data[0]
+
+    except Exception as e:
+        print(e)
+        return {'status': "No Course to match the group"}, 401
 
 def add_user_to_course(userId: int, courseId: int, userRole: Role):
     # check user on course
     # check group on courseä
     # add to group
+    print("heh")
 
 
 def remove_user_from_group(userId: int, groupId: int):
-    #do the doings
+    # do the doings
+    print("heh")
