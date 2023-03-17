fa214068d15522ae7234fa74f6f3512d9d6098c4
Alexander Brunnegård <alex_brunnegard98@hotmail.com>
2023-03-17 13:11:56 +0100

    pep8 import update

--- server/Test/test_login_handler.py
+++ server/Test/test_login_handler.py
@@ -1,5 +1,5 @@
-from src.login_handler import log_in, verify_and_get_token, create_token, create_key, \
-    check_data_input, user_registration, registration_query
+from src.login_handler import log_in, verify_and_get_token, create_token, \
+    create_key, check_data_input, user_registration, registration_query
 import sys
 from pathlib import Path
 import unittest
