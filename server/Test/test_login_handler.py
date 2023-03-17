17c729a0224cf50c50897abc1ca822518780069a
Kvalle99 <sebastian.kvalden@gmail.com>
2023-03-17 15:51:27 +0100

    update a login test that failed

--- server/Test/test_login_handler.py
+++ server/Test/test_login_handler.py
@@ -123,7 +123,7 @@ class TestFileHandler(unittest.TestCase):
 
         result = log_in('test1.chalmers.se', 'pass1')
         self.assertEqual(result[1], 401)
-        self.assertEqual(result[0].get('error'), ("Wrong Credentials"))
+        self.assertEqual(result[0].get('status'), ("wrong_credentials"))
 
     def test_create_and_verify_token(self):
         token = create_token(2)
