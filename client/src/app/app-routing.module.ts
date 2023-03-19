48553db34f114202434b78a8efe246b55726ac38
maltecarlstedt <malte.carlstedt@hotmail.se>
2023-03-19 16:40:05 +0100

    Created a homepage

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   /** THIS path seems to be the reason for the duplication of both the nav and footer.
    * Though, I do not know if this would be a good fix to remove this path entirely.
    * */
-  /* { path: '', component: AppComponent }, */
+  { path: '', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
 ];
 
