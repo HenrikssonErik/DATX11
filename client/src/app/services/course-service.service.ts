deb7068523c7fdad12fd2dc839f5f8059f402805
maltecarlstedt <malte.carlstedt@hotmail.se>
2023-03-22 15:02:33 +0100

    removed old comments

--- client/src/app/services/course-service.service.ts
+++ client/src/app/services/course-service.service.ts
@@ -11,16 +11,10 @@ import { Course } from '../models/courses';
 export class CourseService {
   constructor(private http: HttpClient) {}
 
-  /*   getCourses(): Observable<any> {
-    let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append('Cookies', document.cookie);
-    return this.http.get(`${API_URL}/getCourses`, { headers });
-  } */
-
   getCourses(): Observable<Course[]> {
     const headers = new HttpHeaders().append('Cookies', document.cookie);
-    return this.http.get<Courses>(`${API_URL}/getCourses`, { headers }).pipe(
-      map((res) => res.courses) // access the 'Courses' property of the response
-    );
+    return this.http
+      .get<Courses>(`${API_URL}/getCourses`, { headers })
+      .pipe(map((res) => res.courses));
   }
 }
