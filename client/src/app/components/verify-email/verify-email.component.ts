c3c96d61d444ff64822d249259b09857c8877d59
Alexander Brunnegård <alex_brunnegard98@hotmail.com>
2023-03-28 15:15:25 +0200

    Basic layout for email verification frontend

--- client/src/app/components/verify-email/verify-email.component.ts
+++ client/src/app/components/verify-email/verify-email.component.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { resolve } from 'dns';
 import { API_URL } from 'src/environments/environment';
 
 @Component({
@@ -14,7 +15,10 @@ import { API_URL } from 'src/environments/environment';
   styleUrls: ['./verify-email.component.scss'],
 })
 export class VerifyEmailComponent implements OnInit {
-  cid$: string | undefined;
+  verificationFinished!: Promise<Boolean>;
+  responseMessage: string | undefined;
+  errorCaught!: Promise<Boolean>;
+  imgPath: string = 'emailVerification';
 
   constructor(
     private http: HttpClient,
@@ -36,10 +40,38 @@ export class VerifyEmailComponent implements OnInit {
       .post<HttpResponse<any>>(`${API_URL}/verify_email`, params)
       .subscribe({
         next: (response: any) => {
-          this.cid$ = response['cid'];
-          console.log(response);
+          this.responseMessage = response['cid'];
+
+          setTimeout(() => {
+            this.verificationFinished = Promise.resolve(true);
+            this.imgPath = 'successVerification';
+          }, 2000);
+
+          // this.verificationFinished = new Promise((resolve) => {
+          //   setTimeout(() => resolve(true), 2000);
+          // }).then(() => (this.imgPath = 'successVerification'));
         },
         error: (error) => {
+          let errorStatus = error.error.status;
+          if (errorStatus == 'invalid_verification_token') {
+            this.responseMessage =
+              'The token is invalid. Sign up again to receive a new verification email';
+          } else if (errorStatus === 'expired_verification_signature') {
+            this.responseMessage =
+              'The signature that expired. Sign up again to receive a new verification email!';
+          }
+          // this.responseMessage = error.error.status;
+          console.log(this.responseMessage);
+
+          setTimeout(() => {
+            this.errorCaught = Promise.resolve(true);
+            this.imgPath = 'errorVerification';
+          }, 2000);
+
+          // this.errorCaught = new Promise((resolve) => {
+          //   setTimeout(() => resolve(true), 2000);
+          // }).then(() => (this.imgPath = 'errorVerification'));
+
           console.log(error);
         },
       });
