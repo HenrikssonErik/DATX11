e225dad1d625f527cf5d37e22cc920863fa94f4c
Kvalle99 <sebastian.kvalden@gmail.com>
2023-04-13 21:02:58 +0200

    "coming up" now displays correct deadlines

--- client/src/app/components/courses/courses.component.ts
+++ client/src/app/components/courses/courses.component.ts
@@ -4,6 +4,17 @@ import { User } from 'src/app/models/user';
 import { CourseService } from 'src/app/services/course-service.service';
 import { UserService } from 'src/app/services/user-service.service';
 
+class Deadline {
+  Course!: string;
+  Date!: string;
+
+  constructor(course: string, date: Date) {
+    this.Course = course;
+    console.log(typeof date);
+    this.Date = date.toISOString().substring(0, 10);
+  }
+}
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -12,7 +23,8 @@ import { UserService } from 'src/app/services/user-service.service';
 export class CoursesComponent implements OnInit {
   courses: Course[] = [];
   events = [];
-  deadlines = [];
+  deadlines: Deadline[] = [];
+  labs: number = 0;
   user: User = {
     cid: '',
     email: '',
@@ -32,7 +44,46 @@ export class CoursesComponent implements OnInit {
 
     this.courseService.getCourses().subscribe((res: Course[]) => {
       this.courses = res;
+      this.populateDashboard();
       //console.log(this.courses);
     });
+  }
+
+  populateDashboard(): void {
+    var tempLabs = 0;
+    var tempDeadlines: Deadline[] = [];
+
+    for (var i = 0; i < this.courses.length; i++) {
+      tempLabs = tempLabs + this.courses[i].Assignments.length;
+      tempDeadlines = tempDeadlines.concat(
+        this.findUpcomingAssignments(
+          this.courses[i].Assignments,
+          this.courses[i].Course
+        )
+      );
+    }
+
+    this.labs = tempLabs;
+    this.deadlines = tempDeadlines;
+  }
+
+  findUpcomingAssignments(assignments: any[], course: string): Deadline[] {
+    const deadlines: Deadline[] = [];
+    for (let i = 0; i < assignments.length; i++) {
+      if (this.isDateUpcoming(assignments[i].DueDate)) {
+        deadlines.push(new Deadline(course, new Date(assignments[i].DueDate)));
+      }
+    }
+    return deadlines;
+  }
+
+  isDateUpcoming(date: Date): boolean {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0); //to reset timeframes smaller than days
+    // Check if the date is in the future
+    if (new Date(date).getTime() >= now.getTime()) {
+      return true;
+    }
+    return false;
   }
 }
