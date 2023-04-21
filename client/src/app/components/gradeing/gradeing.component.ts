d0a33f34586b4545fa2e926375692da4b061eb58
maltecarlstedt <malte.carlstedt@hotmail.se>
2023-04-21 11:53:32 +0200

    Progress on gradeing. Adding filtrs and srch

--- client/src/app/components/gradeing/gradeing.component.ts
+++ client/src/app/components/gradeing/gradeing.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from 'src/app/models/courses';
 import { SubmissionService } from 'src/app/services/submission.service';
 
 @Component({
@@ -7,5 +8,15 @@ import { SubmissionService } from 'src/app/services/submission.service';
   styleUrls: ['./gradeing.component.scss'],
 })
 export class GradeingComponent {
+  @Input() course!: Course;
+
   constructor(private submissionService: SubmissionService) {}
+
+  ngOnInit(): void {
+    this.submissionService
+      .getAssignmentOverView(this.course.courseID, 2)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
