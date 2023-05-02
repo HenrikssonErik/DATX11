e5e2c0a26f4811aad1ac19db11cf98657e04f610
Kvalle99 <sebastian.kvalden@gmail.com>
2023-05-02 13:17:47 +0200

    added score to assignment overview

--- server/src/course_handler.py
+++ server/src/course_handler.py
@@ -473,7 +473,7 @@ def get_assignment_overview(course: int) -> list[dict]:
                 assignments = cur.fetchall()
 
                 query_data = """SELECT DISTINCT ON (groupId) groupId, testPass,
-                teacherGrade, submission FROM AssignmentFeedback WHERE courseId = %s
+                teacherGrade, submission, score FROM AssignmentFeedback WHERE courseId = %s
                 AND assignment = %s ORDER BY groupId,
                 submission DESC;"""
                 return_list = []
@@ -487,7 +487,8 @@ def get_assignment_overview(course: int) -> list[dict]:
                                 "groupid": row[0],
                                 "testpass": row[1],
                                 "grade": row[2],
-                                'Submission': row[3]}
+                                'Submission': row[3],
+                                "Score": row[4]}
                             overview_list.append(group_dict)
                     return_list.append({"Assignment": assignment[0],
                                         "Submissions": overview_list})
