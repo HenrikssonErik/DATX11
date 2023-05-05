f7e10ef7b2b4773075b6b5f806f303743f2cf123
maltecarlstedt <malte.carlstedt@hotmail.se>
2023-05-05 14:49:07 +0200

    removed annoing log

--- client/src/app/interceptors/loading.interceptor.ts
+++ client/src/app/interceptors/loading.interceptor.ts
@@ -3,20 +3,21 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { finalize, Observable } from 'rxjs';
 import { LoadingspinnerService } from '../services/loadingspinner.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-
   private totalRequests = 0;
 
   constructor(private loadingService: LoadingspinnerService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('caught')
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     this.totalRequests++;
     this.loadingService.setLoading(true);
     return next.handle(request).pipe(
